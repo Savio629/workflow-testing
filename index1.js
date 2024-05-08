be4c1c392e312a9e0420037ecb1c8a3076f50df4
Savio Dias <91362589+Savio629@users.noreply.github.com>
2024-05-08 23:46:25 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -20,9 +20,10 @@ async function getValue(page, selector) {
         return selectedOption;
     }, selector);
 }
-async function scrapeData() {
+
+async function scrapeData(startDistrict, endDistrict) {
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto('https://ejalshakti.gov.in/JJM/JJMReports/BasicInformation/JJMRep_AbstractData_D.aspx', { waitUntil: 'networkidle0' });
 
@@ -41,7 +42,7 @@ async function scrapeData() {
         // Filter out the "-Select Year-" option
         const validYears = years.filter(year => year.value === "2024-2025" );
 
-         for (let year of validYears) {
+        for (let year of validYears) {
             const yearFolder = path.join(dataDir, year.text.replace(/[\\/:*?"<>|]/g, '-'));
             if (!fs.existsSync(yearFolder)) {
                 fs.mkdirSync(yearFolder);
@@ -54,133 +55,132 @@ async function scrapeData() {
             await page.select('#CPHPage_ddState', '24');
             await new Promise(resolve => setTimeout(resolve, 500));
 
-            // Fetch dropdown options
-            // const states = await getOptions(page, '#CPHPage_ddState');
-            const state = await page.select('#CPHPage_ddState', '24');
-            // console.log(state.text);
-            // for (let state of states) {
-                const stateFolder = path.join(yearFolder, 'Odisha');
-                if (!fs.existsSync(stateFolder)) {
-                    fs.mkdirSync(stateFolder);
+            const stateFolder = path.join(yearFolder, 'Odisha');
+            if (!fs.existsSync(stateFolder)) {
+                fs.mkdirSync(stateFolder);
+            }
+            console.log('Odisha');
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            let districts = await getOptions(page, '#CPHPage_ddDistrict');
+
+            // Limit districts based on input arguments
+            const start = startDistrict || 0;
+            const end = endDistrict || districts.length - 1;
+
+            for (let i = start; i <= end; i++) {
+                const district = districts[i];
+                const districtFolder = path.join(stateFolder, district.text.replace(/[\\/:*?"<>|]/g, '-'));
+                if (!fs.existsSync(districtFolder)) {
+                    fs.mkdirSync(districtFolder);
                 }
-                console.log('Odisha');
-                // await page.select('#CPHPage_ddState', '24');
+                console.log(district.text);
+                await page.select('#CPHPage_ddDistrict', district.value);
                 await new Promise(resolve => setTimeout(resolve, 500));
-                let districts = await getOptions(page, '#CPHPage_ddDistrict');
+                let blocks = await getOptions(page, '#CPHPage_ddBlock');
 
-                for (let district of districts) {
-                    const districtFolder = path.join(stateFolder, district.text.replace(/[\\/:*?"<>|]/g, '-'));
-                    if (!fs.existsSync(districtFolder)) {
-                        fs.mkdirSync(districtFolder);
+                for (let block of blocks) {
+                    const blockFolder = path.join(districtFolder, block.text.replace(/[\\/:*?"<>|]/g, '-'));
+                    if (!fs.existsSync(blockFolder)) {
+                        fs.mkdirSync(blockFolder);
                     }
-                    console.log(district.text);
-                    await page.select('#CPHPage_ddDistrict', district.value);
+
+                    await page.select('#CPHPage_ddBlock', block.value);
                     await new Promise(resolve => setTimeout(resolve, 500));
-                    let blocks = await getOptions(page, '#CPHPage_ddBlock');
+                    let categories = await getOptions(page, '#CPHPage_ddCategory');
 
-                    for (let block of blocks) {
-                        const blockFolder = path.join(districtFolder, block.text.replace(/[\\/:*?"<>|]/g, '-'));
-                        if (!fs.existsSync(blockFolder)) {
-                            fs.mkdirSync(blockFolder);
-                        }
-
-                        await page.select('#CPHPage_ddBlock', block.value);
-                        await new Promise(resolve => setTimeout(resolve, 500));
-                        let categories = await getOptions(page, '#CPHPage_ddCategory');
-
-                        for (let category of categories) {
-                            const categoryNameForFile = category.text.replace(/[\\/:*?"<>|]/g, '-');
-                            await page.select('#CPHPage_ddCategory', category.value);
-                            await new Promise(resolve => setTimeout(resolve, 2000));
-                            const radioButtonsPresent = await page.evaluate(() => {
-                                const radioButtons = document.querySelectorAll('#CPHPage_rdbvillages input[type="radio"]');
-                                return radioButtons.length > 0;
+                    for (let category of categories) {
+                        const categoryNameForFile = category.text.replace(/[\\/:*?"<>|]/g, '-');
+                        await page.select('#CPHPage_ddCategory', category.value);
+                        await new Promise(resolve => setTimeout(resolve, 2000));
+                        const radioButtonsPresent = await page.evaluate(() => {
+                            const radioButtons = document.querySelectorAll('#CPHPage_rdbvillages input[type="radio"]');
+                            return radioButtons.length > 0;
+                        });
+                        if (radioButtonsPresent) {
+                            // Handle radio buttons
+                            const radioButtons = await page.evaluate(() => {
+                                const radios = Array.from(document.querySelectorAll('#CPHPage_rdbvillages input[type="radio"]'));
+                                return radios.map(radio => ({
+                                    id: radio.id,
+                                    value: radio.value,
+                                    label: radio.nextElementSibling.textContent.trim() // Fetch text between <label> tags
+                                }));
                             });
-                            if (radioButtonsPresent) {
-                                // Handle radio buttons
-                                const radioButtons = await page.evaluate(() => {
-                                    const radios = Array.from(document.querySelectorAll('#CPHPage_rdbvillages input[type="radio"]'));
-                                    return radios.map(radio => ({
-                                        id: radio.id,
-                                        value: radio.value,
-                                        label: radio.nextElementSibling.textContent.trim() // Fetch text between <label> tags
-                                    }));
-                                });
 
-                                if (radioButtons.length > 0) {
-                                    // Handle radio buttons if available
-                                    for (let radioButton of radioButtons) {
-                                        try {
-                                            await page.evaluate((id) => {
-                                                const element = document.getElementById(id);
-                                                if (element) {
-                                                    element.click();
-                                                } else {
-                                                    throw new Error(`Element with id ${id} not found.`);
-                                                }
-                                            }, radioButton.id);
+                            if (radioButtons.length > 0) {
+                                // Handle radio buttons if available
+                                for (let radioButton of radioButtons) {
+                                    try {
+                                        await page.evaluate((id) => {
+                                            const element = document.getElementById(id);
+                                            if (element) {
+                                                element.click();
+                                            } else {
+                                                throw new Error(`Element with id ${id} not found.`);
+                                            }
+                                        }, radioButton.id);
 
-                                            // Click on the "Show" button
-                                            await page.click('#CPHPage_btnShow');
-                                            await new Promise(resolve => setTimeout(resolve, 5000));
+                                        // Click on the "Show" button
+                                        await page.click('#CPHPage_btnShow');
+                                        await new Promise(resolve => setTimeout(resolve, 6500));
 
-                                            // Wait for the data to load
-                                            await page.waitForSelector('#tableReportTable', { timeout: 5000 }).catch(() => console.log('Table not found, proceeding to next radio button'));
+                                        // Wait for the data to load
+                                        await page.waitForSelector('#tableReportTable', { timeout: 5000 }).catch(() => console.log('Table not found, proceeding to next radio button'));
 
-                                            const data = await page.evaluate(() => {
-                                                const rows = Array.from(document.querySelectorAll('#tableReportTable tr'));
-                                                return rows.map(row => {
-                                                    const columns = row.querySelectorAll('th, td');
-                                                    return Array.from(columns, column => column.innerText.trim().replace(/\n/g, ' '));
-                                                });
+                                        const data = await page.evaluate(() => {
+                                            const rows = Array.from(document.querySelectorAll('#tableReportTable tr'));
+                                            return rows.map(row => {
+                                                const columns = row.querySelectorAll('th, td');
+                                                return Array.from(columns, column => column.innerText.trim().replace(/\n/g, ' '));
                                             });
+                                        });
 
-                                            // Save to CSV file using radio button label instead of value
-                                            const csvFilePath = path.join(blockFolder, `${radioButton.label}.csv`);
-                                            stringify(data, (err, output) => {
+                                        // Save to CSV file using radio button label instead of value
+                                        const csvFilePath = path.join(blockFolder, `${radioButton.label}.csv`);
+                                        stringify(data, (err, output) => {
+                                            if (err) throw err;
+                                            fs.writeFile(csvFilePath, output, (err) => {
                                                 if (err) throw err;
-                                                fs.writeFile(csvFilePath, output, (err) => {
-                                                    if (err) throw err;
-                                                    // console.log(`${radioButton.label}.csv saved in ${block.text} folder.`);
-                                                });
+                                                // console.log(`${radioButton.label}.csv saved in ${block.text} folder.`);
                                             });
-                                        } catch (error) {
-                                            console.error(error);
-                                            continue;
-                                        }
+                                        });
+                                    } catch (error) {
+                                        console.error(error);
+                                        continue;
                                     }
                                 }
-                            } else {
-                                // Handle categories if radio buttons are not present
-                                await page.click('#CPHPage_btnShow');
-                                await new Promise(resolve => setTimeout(resolve, 5000));
-
-                                // Wait for the data to load
-                                await page.waitForSelector('#tableReportTable', { timeout: 5000 }).catch(() => console.log('Table not found, proceeding to next category'));
-
-                                const data = await page.evaluate(() => {
-                                    const rows = Array.from(document.querySelectorAll('#tableReportTable tr'));
-                                    return rows.map(row => {
-                                        const columns = row.querySelectorAll('th, td');
-                                        return Array.from(columns, column => column.innerText.trim().replace(/\n/g, ' '));
-                                    });
-                                });
-
-                                // Save to CSV file using category name instead of value
-                                const csvFilePath = path.join(blockFolder, `${categoryNameForFile}.csv`);
-                                stringify(data, (err, output) => {
-                                    if (err) throw err;
-                                    fs.writeFile(csvFilePath, output, (err) => {
-                                        if (err) throw err;
-                                        // console.log(`${categoryNameForFile}.csv saved in ${block.text} folder.`);
-                                    });
-                                });
                             }
+                        } else {
+                            // Handle categories if radio buttons are not present
+                            await page.click('#CPHPage_btnShow');
+                            await new Promise(resolve => setTimeout(resolve, 5000));
+
+                            // Wait for the data to load
+                            await page.waitForSelector('#tableReportTable', { timeout: 5000 }).catch(() => console.log('Table not found, proceeding to next category'));
+
+                            const data = await page.evaluate(() => {
+                                const rows = Array.from(document.querySelectorAll('#tableReportTable tr'));
+                                return rows.map(row => {
+                                    const columns = row.querySelectorAll('th, td');
+                                    return Array.from(columns, column => column.innerText.trim().replace(/\n/g, ' '));
+                                });
+                            });
+
+                            // Save to CSV file using category name instead of value
+                            const csvFilePath = path.join(blockFolder, `${categoryNameForFile}.csv`);
+                            stringify(data, (err, output) => {
+                                if (err) throw err;
+                                fs.writeFile(csvFilePath, output, (err) => {
+                                    if (err) throw err;
+                                    // console.log(`${categoryNameForFile}.csv saved in ${block.text} folder.`);
+                                });
+                            });
                         }
                     }
                 }
-             }
-        // }
+            }
+        }
 
         await browser.close();
     } catch (err) {
@@ -188,4 +188,8 @@ async function scrapeData() {
     }
 }
 
-scrapeData();
+// Extract start and end districts from command line arguments
+const startDistrict = parseInt(process.argv[2]);
+const endDistrict = parseInt(process.argv[3]);
+
+scrapeData(startDistrict, endDistrict);
